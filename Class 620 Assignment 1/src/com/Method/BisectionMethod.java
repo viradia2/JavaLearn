8e37bc5060aec25d1bbe828a53984203f9ea8dc6
vvviradia <viradia2@marshall.edu>
2016-09-01 01:17:05 -0400

    Updated

--- Class 620 Assignment 1/src/com/Method/BisectionMethod.java
+++ Class 620 Assignment 1/src/com/Method/BisectionMethod.java
@@ -1,15 +1,14 @@
 package com.Method;
 
-
 public class BisectionMethod {
 
-	private static final double EPSILON = 0;
+	private static final double EPSILON = 0.000001;
 	
 	public static void main(String[] args) {
 		
 		double a=0, b=100;
-				
-		System.out.printf("The root is: %.4f",bisection(a,b,EPSILON));
+					
+		System.out.printf("The root by Bisetction Method is: %.4f",bisection(a,b,EPSILON));
 		
 	}
 
@@ -24,28 +23,30 @@ public class BisectionMethod {
 		if(f(b) == 0)
 			return 0;
 		
-		if(f(mid)<EPSILON)
+		if(Math.abs(f(mid))<EPSILON)
 			return mid;
 		
-		while(f(mid)>EPSILON){
+		while(Math.abs(f(mid)) > EPSILON){
+			
 			System.out.printf("Step: %2d a:%.4f  b:%.4f  mid:%.4f  f(mid):%.4f \n ",i,a,b,mid,f(mid));
 			
-			if(f(a)*f(b) < 0)
+			if(f(a) * f(mid) < 0)
 				b = mid;
 			else 
-				a = mid;
+				a = mid; 
 			
-			mid = (a+b)/2;
+			mid = (a + b)/2;
 			i++;
 	
 		}
+		
 		return mid;
 		
 	}
 	
 	private static double f(double x){
 		
-		return Math.pow(x, 2) - 5;
+		return 30 + 25 * x - 4 * Math.pow(x, 2) + 0.15 * Math.pow(x, 3);
 	}
 	
 }
